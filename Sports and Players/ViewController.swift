9a184be66ef45e767cead2dbe0432c9702cd65b8
faisal alfariheedi <falfariheedi@gmail.com>
2021-12-20 10:31:26 +0300

    Initial Commit

---
+++ Sports and Players/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sports and Players
+//
+//  Created by faisal on 16/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
